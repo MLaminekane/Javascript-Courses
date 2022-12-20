c5048684918cab04cfd58f3ed1dedbd3dbf10bb9
Lamine Kane <mlaminekane01@gmail.com>
2022-12-20 23:32:55 +0100

    finalization

--- form-validation/script.js
+++ form-validation/script.js
@@ -9,63 +9,66 @@
 
 // //recuperer un element a manipuler
 // const username = document.querySelector('username')//mm chose
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const password2 = document.getElementById('password2')
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-form.addEventListener('submit',e => {
-    e.preventDefault();
-    checkInputs();
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
 });
 
-function checkInputs(){
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-
-    if (usernameValue === ''){
-        setErrorFor(username, 'remplisser la case')
-    }else{
-        setSuccessFor(username)
-    }
-
-    if (emailValue === ''){
-        setErrorFor(email, 'remplisser la case')
-    }else if (!isEmail(emailValue)){
-        setErrorFor(email,'remplisser la case')
-    }else{
-        setSuccessFor(email)
-    }
-
-    if (passwordValue === ''){
-        setErrorFor(password,'remplisser la case')
-    }else{
-        setSuccessFor(password)
-    }
-
-    if (password2Value === ''){
-        setErrorFor(password2,'remplisser la case')
-    }else if (passwordValue !== password2Value){
-        setErrorFor(password2,"password not correct")
-    }else{
-        setSuccessFor(password2)
-    }
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username);
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email);
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email);
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password);
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2);
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2);
+	} else{
+		setSuccessFor(password2);
+	}
 }
 
-function setErrorFor(input, message){
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small')
-    formControl.classNAme = 'form-control error';
-    small.innerText = message;
-}
-function setSuccessFor(input){
-    const formControl = input.parentElement;
-    formControl.classNAme = 'form-control success';
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
 }
 
-function isEmail(email){
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
